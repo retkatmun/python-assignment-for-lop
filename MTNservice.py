ba0eb51b4c9f642331aac1e520679d842f7e073b
Retkatmun <retkatmun1@gmail.com>
2025-08-14 02:40:02 +0000

    first commit

---
+++ MTNservice.py
@@ -0,0 +1,14 @@
+#M.T.N Sevive
+
+balance = 1000
+borrow_limit = 500
+dept = 0
+
+ussd_code = input("USSD Code:...")
+
+while True:
+    print("1. check balance: ")
+    print("2. Buy data: ")
+    print("3, Buy airtime: ")
+    print("4. Borrow service: ")
+    break
